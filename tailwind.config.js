1caa1d2fb24a7cb5345dbe537fbc9bfc4b48de47
Jeong <hj08hn@gmail.com>
2023-06-19 10:01:20 +0900

    Add: add custom theme
    - tailwind.config.js에 폰트, 컬러, 박스쉐도우 theme을 추가합니다
    
    by Hah-nna #5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        noto: ['Noto Sans KR'],
+        noto: ['Noto Sans KR', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
         sans: ['Graphik', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
       },
@@ -13,8 +14,13 @@ export default {
         beige: '#F6F4F1',
         gray: '#999999',
         'beige-dark': '#F6F4F1',
+        tag: '#F1EEEA',
+        red: '#FF4040',
       },
       fontSize: {},
+      boxShadow: {
+        basic: '0px 4px 8px rgba(41, 41, 41, 0.08)',
+      },
     },
   },
   plugins: [],
