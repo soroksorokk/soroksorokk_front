09b350b38fdb3b30d0a752c221b9bf7014265e31
j1y0ung <j1youngyoon94@gmail.com>
2023-06-20 12:47:47 +0900

    Design: add my profile part * add some my profile toggle part @j1y0ungyoon #7

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,47 +2,55 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div className="flex flex-auto">
+    <div className="flex p-4 ">
       <div
-        className=" box-border flex flex-col items-center border"
+        className=" box-border flex flex-col items-center bg-white"
         style={{ width: '430px', height: '638px', borderRadius: '40px' }}
       >
         <div className="flex w-full flex-col">
           <h1
-            className="p-6 pb-3 text-3xl font-semibold"
+            className="p-6 pb-3 text-3xl font-semibold "
             style={{ paddingLeft: '14px' }}
           >
             프로필
           </h1>
-          <hr className="border-1 border-gray" />
+          <hr className="border-1 border-gray " />
         </div>
         <div className="flex w-full flex-col items-center py-6">
-          <img className="h-64 w-64 rounded-full border-2" />
+          <img
+            src="images/sample/soondoo.jpeg"
+            className=" border-1 mb-6 h-64 w-64 rounded-full object-cover"
+          />
           <div>
-            <button className="m-6">팔로우 20</button>
-            <button>팔로워 10</button>
+            <button className="m-3">팔로우20</button>
+            <button className="text-purple">팔로워10</button>
           </div>
-          <button className="flex h-9 w-24 rounded-full bg-purple px-4 py-2 text-sm text-white">
-            게시글보기
-          </button>
+          <div className="flex flex-row">
+            <button className="btn-small">게시글 보기</button>
+
+            {/* <button className="btn-small">이미지 제거</button>
+            <button className="btn-small bg-beige-dark text-gray">
+              이미지 수정
+            </button> */}
+          </div>
           <div className="mb-2 mt-2 flex w-full flex-col items-start px-8 ">
-            <h2 className="mb-4 font-bold ">닉네임</h2>
+            <div className=" mb-2 flex w-[100%] flex-row justify-between">
+              <h2 className="font-bold">닉네임</h2>
+              {/* 내 프로필 수정 버튼 */}
+              {/* <button className="flex align-top text-gray underline">
+                수정
+              </button> */}
+            </div>
             <div className="flex flex-row">
-              <div
-                className="mr-2 flex w-20 items-center rounded-xl border border-black bg-beige-dark"
-                style={{ width: '74px', padding: '1.5px 8px 1.5px 8px' }}
-              >
-                <p className="w-full">#tag</p>
+              <div className="tag">
+                <p className="w-full text-purple">#tag</p>
               </div>
-              <div
-                className=" mr-2 w-20 rounded-xl border border-black bg-beige-dark"
-                style={{ width: '74px', padding: '1.5px 8px 1.5px 8px' }}
-              >
-                <p>#tag</p>
+              <div className="tag">
+                <p className="w-full text-purple">#tag</p>
               </div>
             </div>
 
-            <h2 className="my-4 font-bold">한줄소개</h2>
+            <h2 className="my-2 font-bold">한줄소개</h2>
             <div className="mb-6"> 나는 슬플 때 노래를 듣지</div>
           </div>
         </div>
