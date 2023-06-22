65e2d80eb33a86bcab2b83188ae0e96035b64af4
Jeong <hj08hn@gmail.com>
2023-06-22 18:37:46 +0900

    Comment: add termsofuse component comment
    - termsofuse 컴포넌트에 있는 함수handleAgreeCheck와 handleAllAgreementCheck에 대해서 주석을 답니다
    
    by @Hah-nna #6

--- src/components/Modal/TermsOfUse.tsx
+++ src/components/Modal/TermsOfUse.tsx
@@ -3,8 +3,10 @@ import Button from '../../UI/Button';
 import { ReactComponent as AgreeSign } from '../../assets/AgreeSign.svg';
 
 const TermsOfUse = () => {
-  /* 전체 동의 state, 개별 동의 state */
+  /* 전체 동의 state */
   const [allAgree, setAllAgree] = useState(false);
+
+  /* 개별 동의 state */
   const [agree, setAgree] = useState({
     termsAgree: false,
     personalInfoAgree: false,
@@ -24,23 +26,51 @@ const TermsOfUse = () => {
 
   /* 전체 동의 state 변경 함수 */
   const handleAgreeCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.target에서 e.target.name을 {name}으로, e.target.checked을 구조분해 할당으로 가져옴
+    // e.target.name은 체크박스의 name을 가져옴
+    // e.target.checked는 체크박스가 체크되었는지 아닌지를 boolean으로 알려줌
     const { name, checked } = e.target;
     setAgree((preAgree) => ({ ...preAgree, [name]: checked }));
+    // 만약 termsAgree가 checked 되었다면(true라면) ...preAgree로 이전 agree들을 복사하고
+    // termsAgree: true로 업데이트한 새로운 객체를 생성함
 
     const allChecked = Object.values({ ...agree, [name]: checked }).every(
       (value) => value === true,
     );
+    // Object.values로 이전의 agree 객체에서 클릭한 요소(name)에 checked를 업데이트하고
+    // value만 뽑아 배열로 바꿈
+    // ex: [true, false, true, true, false]
+    // 다시 업데이트 하는 이유는 agree의 값이 바로 업데이트 되지 않기 때문임
+    // 바로 업데이트 안 되는 이유 : 상태 업데이트 함수(setAgree)가 호출되면
+    // 업데이트를 스케줄링하고 나중에 컴포넌트 렌더링이 발생하면 그때 값이 업데이트 되기 때문임
+
+    // 그 다음 배열 메소드인 every를 사용해 배열의 모든 값이 true인지 확인 후
+    // setAllgree를 호출해 allAgree 상태를 업데이트 함
+
     setAllAgree(allChecked);
   };
 
   const handleAllAgreementCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = e.target;
+    // e.target에서 e.target.checked을 구조분해 할당으로 가져옴
+
     const newAgreements = Object.keys(agree).reduce((newAgree, agreeKey) => {
+      // Object.keys로 agree 객체에서 key만 뽑아 배열로 만듦
+      // ex: [termsAgree, personalInfoAgree, optionalPersonalInfoAgree, salesInfoAgree, ageAgree]
       newAgree[agreeKey] = checked;
+      // reduce 함수를 사용해 agree를 순회하면서 요소 하나하나를 newAgree(초기값이 빈 배열) 속에
+      // 새로운 키(agreeKey)로 추가하고 value로 변수 checked의 값을 할당함(모두 동의 버튼이 눌렸으니까 checked는 true)
+      // ex: newAgree[termsAgree] = true ->
+      // newAgree = {termsAgree: true,} -> newAgree[ageAgree] = true -> newAgree = {termsAgree: true, ageAgree: true}
+      // 즉, 순회하면서 변수 checked의 값인 true를 각 키의 값으로 할당함
+      // 그럼 모든 약관 동의 요소의 checked가 true로 바뀜
       return newAgree;
+      // 각 키에 대한 value 값이 true인 새로운 객체를 리턴함
     }, {} as agreeProps);
     setAgree(newAgreements);
+    // agree 상태를 업데이트
     setAllAgree(checked);
+    // allAgree 상태를 업데이트
   };
 
   /* 항목별 text */
@@ -195,40 +225,3 @@ const TermsOfUse = () => {
 };
 
 export default TermsOfUse;
-
-// 스테이트
-// const [allAgreed, setAllAgreed] = useState(false);
-// const [agreements, setAgreements] = useState({
-//   termsAgreed: false,
-//   personalInfoAgreed: false,
-//   provisionAgreed: false,
-//   locationAlarmAgreed: false,
-//   serviceAlarmAgrred: false,
-// });
-
-// 이벤트 핸들러
-// const handleAgreementChange = (e: any) => {
-//   const { name, checked } = e.target;
-
-//   setAgreements((preAgreements: any) => ({...preAgreements, [name]:checked}))
-//   const allChecked = Object.values({...agreements, [name]: checked})
-//   .every((val: any) => val === true);
-//   setAllAgreed(allChecked)
-
-// }
-
-// const handleAllAgreementChange = (e: any) => {
-//   const { checked } = e.target;
-//   setAgreements((preAgreements) => Object.keys(preAgreements).reduce((...newAgreements, agreementKey) => ({
-//     ...newAgreements,
-//     [agreementKey] : checked
-//   }),
-//   {}
-//   )
-//   )
-//   setAgreements(checked)
-// }
-
-// // jsx
-// <input type='checkbox' id='agree_check_all' name='agree_check_all' checked={allAgreed}
-// onChange={handle} />
