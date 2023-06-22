9763d64c0ab0d1db49dd465a2b3eed6667378b4f
Jeong <hj08hn@gmail.com>
2023-06-22 18:35:42 +0900

    Comment: add button component comment
    - Button 컴포넌트에 관한 설명을 적어놓았습니다
    
    by @Hah-nna #5 #6

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { ButtonProps } from '../type/type';
 
 const Button = ({ children, onClick, className }: ButtonProps) => {
+  /**
+   * 버튼 컴포넌트 import 해서 사용할 때 className은 아래와 같이 사용하면 됨
+   * <Button className={'btn-purple' + ' mb-6 text-white 내가 지정할 속성들'}></Button>
+   */
   return (
     <button className={'btn-purple ' + `${className}`} onClick={onClick}>
       {children}
