02465e700f8fe28e35d5f6bb27f4c735ffb9c327
Jeong <hj08hn@gmail.com>
2023-06-22 18:42:13 +0900

    Comment: add postcardBox component comment
    - postcardBox 컴포넌트가 무슨 역할을 하는지에 대해 주석을 답니다
    
    by @Hah-nna #5

--- src/components/mainPage/PostCardBox.tsx
+++ src/components/mainPage/PostCardBox.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { ChildrenProps } from '../../type/type';
 
 const PostCardBox = ({ children }: ChildrenProps) => {
+  /**
+   * post card list가 담기는 컴포넌트임
+   */
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
       {children}
