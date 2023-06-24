dfcc3bca619c071f06a10ac323f07ba7981f2c33
j1y0ung <j1youngyoon94@gmail.com>
2023-06-25 01:52:20 +0900

    Feat: show and fold reply comment function * can show and fold reply comment @j1y0ungyoon

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReplyComment from './ReplyComment';
 const Comment = () => {
+  const [isComment, setIsComment] = useState(true);
+
   return (
     <div className="flex w-[100%] flex-row  ">
       <div className="flex w-[15%] justify-end">
@@ -15,13 +17,13 @@ const Comment = () => {
           <p className="m-1 font-normal text-gray">2023.06.20</p>
         </div>
         <p className="m-1">최고최고 짱짱 </p>
-        <div className="flx-row flex items-center">
+        <div className=" flx-row flex items-center">
           <button className="h-7 w-7 bg-[url('/images/sample/thumbsUp.svg')]"></button>
           <p>2</p>
           <button className="m-2">답글</button>
         </div>
 
-        <ReplyComment />
+        {isComment ? <ReplyComment /> : <div className=" hidden " />}
       </div>
     </div>
   );
--- src/components/ReplyComment.tsx
+++ src/components/ReplyComment.tsx
@@ -1,42 +1,55 @@
 import React, { useState } from 'react';
 
 const ReplyComment = () => {
-  const [isComment, setIsComment] = useState(false);
+  const [isMoreComment, setIsMoreComment] = useState(false);
+
   const onClickMoreCommentButton = () => {
-    setIsComment(!isComment);
+    setIsMoreComment(!isMoreComment);
   };
-  console.log(isComment);
+
   return (
     <div className="flex w-[100%] flex-col ">
-      <div className="flex flex-row items-center">
-        <div>
-          <button
-            className="h-4 w-4 bg-upward  bg-center bg-no-repeat"
+      <div>
+        {!isMoreComment ? (
+          <div
+            className="flex w-[100px] cursor-pointer flex-row items-center"
             onClick={() => onClickMoreCommentButton()}
-          />
-          <p className="m-2 font-bold text-purple">답글</p>
-          <p className=" font-bold text-purple">2개</p>
-        </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="flex w-[15%] justify-end">
-          <img
-            src="/images/sample/soondoo.jpeg"
-            className=" h-[46px] w-[46px] rounded-full border object-cover"
-          />
-        </div>
-        <div className="w-[85%] ">
-          <div className="m-2 flex flex-row ">
-            <p className="m-1 font-semibold">닉네임</p>
-            <p className="m-1 font-normal text-gray">2023.06.20</p>
+          >
+            <button className="h-4 w-4 bg-upward  bg-center bg-no-repeat" />
+            <p className="m-2 font-bold text-purple">답글</p>
+            <p className=" font-bold text-purple">2개</p>
           </div>
-          <p className="m-1">최고최고 짱짱 </p>
-          <div className="flx-row flex items-center">
-            <button className="h-7 w-7 bg-[url('/images/sample/thumbsUp.svg')]"></button>
-            <p>2</p>
-            <button className="m-2">답글</button>
+        ) : (
+          <div onClick={() => onClickMoreCommentButton()}>
+            <div className="flex flex-row items-center ">
+              <button className="h-4 w-4 bg-downward bg-center bg-no-repeat" />
+
+              <p className="m-2 font-bold text-purple">답글</p>
+              <p className=" font-bold text-purple">2개</p>
+            </div>
+
+            <div className="flex w-[100%] flex-row  ">
+              <div className="flex w-[15%] justify-end">
+                <img
+                  src="/images/sample/soondoo.jpeg"
+                  className=" h-[46px] w-[46px] rounded-full border object-cover"
+                />
+              </div>
+              <div className="w-[85%] ">
+                <div className="m-2 flex flex-row ">
+                  <p className="m-1 font-semibold">닉네임</p>
+                  <p className="m-1 font-normal text-gray">2023.06.20</p>
+                </div>
+                <p className="m-1">최고최고 짱짱 </p>
+                <div className=" flx-row flex items-center">
+                  <button className="h-7 w-7 bg-[url('/images/sample/thumbsUp.svg')]"></button>
+                  <p>2</p>
+                  <button className="m-2">답글</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
