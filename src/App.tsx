8a5b1f3291801c861231d7039da6416c7959a3bf
j1y0ung <j1youngyoon94@gmail.com>
2023-06-26 13:21:23 +0900

    Fix: fix App.tsx to origin version * fix App.tsx @j1y0ungyoon

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PostListNav from './components/mainPage/PostListNav';
 import PostCard from './components/mainPage/PostCard';
 import PostCardBox from './components/mainPage/PostCardBox';
 import { CategoryEmoji } from '../src/type/type';
+
 function App() {
   const [isCategoryShow, setIsCategoryShow] = useState(false);
   const [categoryEmojis, setCategoryEmojis] = useState<{
@@ -18,12 +19,22 @@ function App() {
   });
 
   return (
-    <div className="m-3 h-full w-full bg-beige underline">
-      베이지 부분은 커스텀 theme이랑 일반 tailwind 적용한거임
-      <button className="btn-purple">
-        보라 부분은 커스텀 클래스 적용한거임
-      </button>
-    </div>
+    <>
+      <MainLayout>
+        <MainPostListBox>
+          <PostListHeader />
+          <PostListNav
+            isCategoryShow={isCategoryShow}
+            setIsCategoryShow={setIsCategoryShow}
+            categoryEmojis={categoryEmojis}
+            setCategoryEmojis={setCategoryEmojis}
+          />
+          <PostCardBox>
+            <PostCard />
+          </PostCardBox>
+        </MainPostListBox>
+      </MainLayout>
+    </>
   );
 }
 
