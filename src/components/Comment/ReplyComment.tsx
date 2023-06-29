73895b0f735a7e11fd2ef35b7069b6fed9039484
j1y0ung <j1youngyoon94@gmail.com>
2023-06-29 17:42:16 +0900

    Fix: fix px to rem * fix px to rem * change file directory @j1y0ungyoon

--- src/components/Comment/ReplyComment.tsx
+++ src/components/Comment/ReplyComment.tsx
@@ -13,7 +13,7 @@ const ReplyComment = () => {
       <div>
         {!isMoreComment ? (
           <div
-            className="flex w-[100px] cursor-pointer flex-row items-center"
+            className="flex w-[6.25rem] cursor-pointer flex-row items-center"
             onClick={() => onClickMoreCommentButton()}
           >
             <button className="h-4 w-4 bg-upward  bg-center bg-no-repeat" />
@@ -29,21 +29,21 @@ const ReplyComment = () => {
               <p className=" font-bold text-purple">2개</p>
             </div>
 
-            <div className="flex w-[100%] flex-row  ">
-              <div className="flex w-[15%] justify-end">
+            <div className="flex w-[100%] flex-row justify-start ">
+              <div>
                 <img
-                  src="/images/sample/soondoo.jpeg"
-                  className=" h-[46px] w-[46px] rounded-full border object-cover"
+                  src="/assets/soondoo.jpeg"
+                  className=" h-[2.875rem] w-[2.875rem] rounded-full border object-cover"
                 />
               </div>
-              <div className="w-[85%] ">
+              <div>
                 <div className="m-2 flex flex-row ">
                   <p className="m-1 font-semibold">닉네임</p>
                   <p className="m-1 font-normal text-gray">{today}</p>
                 </div>
                 <p className="m-1">최고최고 짱짱 </p>
                 <div className=" flx-row flex items-center">
-                  <button className="h-7 w-7 bg-[url('/images/sample/thumbsUp.svg')]"></button>
+                  <button className="h-7 w-7 bg-[url('/assets/thumbsUp.svg')]"></button>
                   <p>2</p>
                   <button className="m-2">답글</button>
                 </div>
