cede87de0db8163f8e67b6224d96a4091abd891b
Jeong <hj08hn@gmail.com>
2023-07-01 20:49:16 +0900

    Add: add media query
    - 미디어 쿼리(휴대폰, 태블렛, 노트북, 데스크탑 추가) 추가합니다
    
    by @Hah-nna #5

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,19 @@ export default {
         light: '0px 4px 4px rgba(0, 0, 0, 0.25);',
       },
     },
+    screens: {
+      mobile: '320px',
+      // => @media (min-width: 320px)
+
+      tablet: '640px',
+      // => @media (min-width: 640px)
+
+      notebook: '1024px',
+      // => @media (min-width: 1024px)
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px)
+    },
   },
   plugins: [
     plugin(function ({ addUtilities }) {
