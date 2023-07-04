0912668d91b8abb54931f02e7f350f9cb4f20e31
Jeong <hj08hn@gmail.com>
2023-07-04 21:46:15 +0900

    Comment: edit comment
    - 클래스를 지정하는 주석을 수정합니다
    
    by @Hah-nna

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonProps } from '../type/type';
 const Button = ({ children, onClick, className }: ButtonProps) => {
   /**
    * 버튼 컴포넌트 import 해서 사용할 때 className은 아래와 같이 사용하면 됨
-   * <Button className={'btn-purple' + ' mb-6 text-white 내가 지정할 속성들'}></Button>
+   * <Button className="btn-purple mb-6 text-white 내가 지정할 속성들"></Button>
    * 클래스명이 있으면 ${className}으로 지정 되도록 수정 default는 btn-purple - 태영
    */
   return (
