745f5fbaf7ac426b807074f538e3f889784c0bcc
Jeong <hj08hn@gmail.com>
2023-07-06 17:24:55 +0900

    Fix: flex-wrap -> flex-col
    - flex-wrap을 flex-col으로 변경합니다
    
    by @Hah-nna

--- src/UI/PostDetailPage.tsx
+++ src/UI/PostDetailPage.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from '../type/type';
 
 const PostDetailPage = ({ children }: ChildrenProps) => {
   return (
-    <div className="flex h-full w-[27.125rem] flex-wrap rounded-[2.5rem] bg-white tablet:w-[21rem] notebook:w-[25rem] desktop:w-[27.125rem] ">
+    <div className="flex h-full w-[27.125rem] flex-col rounded-[2.5rem] bg-white tablet:w-[21rem] notebook:w-[25rem] desktop:w-[27.125rem] ">
       {children}
     </div>
   );
