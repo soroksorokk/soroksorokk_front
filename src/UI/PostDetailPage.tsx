135812fc4d86224aa77ede9bf17d1f8a4f9497ea
young <75434044+young-02@users.noreply.github.com>
2023-07-06 16:30:38 +0900

    Modify word
    오타 수정

--- src/UI/PostDetailPage.tsx
+++ src/UI/PostDetailPage.tsx
@@ -3,7 +3,7 @@ import { ChildrenProps } from '../type/type';
 
 const PostDetailPage = ({ children }: ChildrenProps) => {
   return (
-    <div className="f mx-6 flex h-full w-[27.125rem] flex-col rounded-[2.5rem] bg-white ">
+    <div className="mx-6 flex h-full w-[27.125rem] flex-col rounded-[2.5rem] bg-white ">
       {children}
     </div>
   );
