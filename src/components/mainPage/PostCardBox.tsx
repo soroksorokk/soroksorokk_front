d646ab2d488364857d6248e0cba1224713e20414
Jeong <hj08hn@gmail.com>
2023-07-10 22:04:38 +0900

    Design: edit grid
    - 디테일포스트오픈이 true이면 grid가 2개로 바뀌게 변경합니다
    
    by @Hah-nna #5

--- src/components/mainPage/PostCardBox.tsx
+++ src/components/mainPage/PostCardBox.tsx
@@ -11,7 +11,9 @@ const PostCardBox = ({ children }: ChildrenProps) => {
   return (
     <div
       className={`mt-3 grid grid-cols-3 grid-rows-2 gap-4 px-[1.8125rem] mobile_xs:grid-cols-1 mobile_sm:grid-cols-1 ${
-        isDetailPostOpen ? 'tablet:grid-cols-1' : 'tablet:grid-cols-2'
+        isDetailPostOpen
+          ? 'tablet:grid-cols-1 desktop:grid-cols-2'
+          : 'tablet:grid-cols-2 desktop:grid-cols-3'
       } notebook:grid-cols-2 desktop:notebook:grid-cols-3`}
     >
       {children}
