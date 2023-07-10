96962009896925cf34676c462eb1d63c121c3842
Jeong <hj08hn@gmail.com>
2023-07-10 22:06:28 +0900

    Fix: change closeBtn components to img
    - 리액트 컴포넌트로 import 해온 svg 파일을 img로 변경합니다
    
    by @Hah-nna

--- src/components/Modal/FollowModal.tsx
+++ src/components/Modal/FollowModal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import useModal from '../../hook/useModal';
 import ModalBackground from '../../UI/ModalBackground';
 
-import { ReactComponent as CloseBtn } from '../../../public/assets/closeBtn.svg';
 interface FollowModalProps {
   title?: string;
 }
@@ -17,7 +16,8 @@ const FollowModal = (title: FollowModalProps) => {
       <div className="modal-follow-box" onClick={(e) => e.stopPropagation()}>
         <header className="m-4 flex flex-row justify-between">
           <h1 className="text-2xl font-bold">팔로워</h1>
-          <CloseBtn
+          <img
+            src="/assets/closeBtn.svg"
             width={23}
             height={23}
             onClick={onClose}
