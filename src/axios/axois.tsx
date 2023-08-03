0825507cf5b0c38a2db638794f55a8526ca1c378
Hah-nna <hj08hn@gmail.com>
2023-08-03 23:30:41 +0900

    Feat: axios instance and interceptors
    - axios의 인스턴스를 생성합니다
    
    - 인터셉터를 적용하여 response와 request를 완료하기 전에 헤더와 토큰여부를 확인하는 로직을 구성합니다
    
    by @Hah-nna

---
+++ src/axios/axois.tsx
@@ -0,0 +1,78 @@
+import axios from 'axios';
+// const basicApi = axios.create({
+//   baseURL: import.meta.env.VITE_APP_PUBLIC_KEY
+// })
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_PUBLIC_KEY,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('userToken');
+
+    if (!token) {
+      config.headers['authorization'] = null;
+      config.headers['authorization-refresh'] = null;
+    } else {
+      const { authorization, 'authorization-refresh': authorizeRefresh } =
+        JSON.parse(token);
+      config.headers['authorization'] = `Bearer ${authorization}`;
+      config.headers['authorization-refresh'] = `Bearer ${authorizeRefresh}`;
+    }
+    console.log('config.headers', config.headers);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    // 값이 정상적으로 들어왔을 때(200번대 응답)
+    return response;
+  },
+
+  async function (error) {
+    // 에러일 경우 처리(200번대 응답이 아닐 경우)
+
+    const originConfig = error.config;
+
+    if (error.response.status === 403) {
+      const refreshToken = originConfig.headers['authorization-refresh'];
+      try {
+        const data = await axios({
+          url: '/api/auth/re-issue',
+          method: 'POST',
+          headers: {
+            'authorization-refresh': refreshToken,
+          },
+        });
+
+        // const data = await basicApi.post('/api/auth/re-issue', {
+        //   headers: { 'Authorization-Refresh': refreshToken },
+        // });
+
+        if (data) {
+          localStorage.setItem(
+            'token',
+            JSON.stringify(data.data, [
+              'authorization',
+              'authorization-refresh',
+            ]),
+          );
+          return await instance.request(originConfig);
+        }
+      } catch (error) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
