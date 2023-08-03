2ea216ebe7f17a7f6c7cc507073bed2c40223da8
Hah-nna <hj08hn@gmail.com>
2023-08-03 23:26:42 +0900

    Feat:apply react-router-dom v6.4
    - 리액트 라우터돔 v6.4를 적용합니다
    
    by @Hah-nna

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import {
   RecoilRoot,
@@ -9,27 +8,12 @@ import {
   useRecoilState,
   useRecoilValue,
 } from 'recoil';
-import ErrorPage from './routes/ErrorPage';
-import NewPostPage from './routes/NewPostPage';
 import App from './App';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/newPost/:postId',
-    element: <NewPostPage />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <RouterProvider router={router} />
+      <App />
     </RecoilRoot>
   </React.StrictMode>,
 );
