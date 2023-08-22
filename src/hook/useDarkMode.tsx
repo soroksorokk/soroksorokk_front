0a9b2a8bd771da4eb6521454b978f015533f2cde
Hah-nna <hj08hn@gmail.com>
2023-08-22 19:15:02 +0900

    Feat: custom darkmode hook
    - state의 상태에 따라 index.html의 html class에 dark가 추가될 수 있게 합니다
    
    - 새로고침을 해도 상태를 유지할 수 있게 로컬스토리지를 사용합니다
    
    by @Hah-nna #25

--- src/hook/useDarkMode.tsx
+++ src/hook/useDarkMode.tsx
@@ -1,42 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRecoilState } from 'recoil';
 import { darkModeState } from '../store/isDarkModeState';
 
 const useDarkMode = () => {
   const [isDark, setIsDark] = useRecoilState(darkModeState);
 
-  // 다크 모드 설정 시스템 설정 값에 의관한 결과 반환
-  useEffect(() => {
-    const setDarkModeFromSystemPreferences = () => {
-      if (
-        !('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches
-      ) {
-        setIsDark(true);
+  const handleToggleDarkMode = () => {
+    setIsDark((prevState: boolean) => {
+      const prevDarkModeState = !prevState;
+
+      if (prevDarkModeState) {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add('dark');
       } else {
-        setIsDark(localStorage.theme === 'dark');
+        document.documentElement.classList.remove('dark');
+        localStorage.removeItem('theme');
       }
-    };
-
-    setDarkModeFromSystemPreferences();
-  }, []);
-
-  // 다크 모드 상태가 변경되면 localStorage와 HTML 요소에서도 반영
-  useEffect(() => {
-    if (isDark || !('theme' in localStorage)) {
-      localStorage.setItem('theme', 'dark');
-      document.documentElement.classList.add('dark-mode');
-    } else {
-      localStorage.removeItem('theme');
-      document.documentElement.classList.remove('dark-mode');
-    }
-  }, [isDark]);
-
-  const darkModeToggleHandler = () => {
-    setIsDark(!isDark);
+      return prevDarkModeState;
+    });
   };
 
-  return { isDark: isDark, toggleDarkMode: darkModeToggleHandler };
+  return { isDark, setIsDark, handleToggleDarkMode };
 };
-
 export default useDarkMode;
