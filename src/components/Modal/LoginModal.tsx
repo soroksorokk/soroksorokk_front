eaecad966334cc1dc33ab3f735c78cbaaca3fc08
j1y0ung <j1youngyoon94@gmail.com>
2023-08-23 17:01:18 +0900

    Fix: import follow.tsx into FollowModal.tsx import follow componet on FollowModal Component and remove duplicated modal frame @j1y0ungyoon

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -1,69 +1,28 @@
+import React from 'react';
 import useModal from '../../hook/useModal';
 import ModalBackground from '../../UI/ModalBackground';
 import Button from '../../UI/Button';
 import { ReactComponent as GoogleLogin } from '../../assets/googleIcon.svg';
 import { ReactComponent as GitHubLogin } from '../../assets/githubIcon.svg';
 import useWidthResize from '../../hook/useWidthResize';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import isLoggedInState from '../../store/isLoggedInState';
-import { useMutation } from '@tanstack/react-query';
-import { onSubmitHandler } from '../../api/reactQueryApis';
 
 export interface LoginModalProps {
   title?: string;
   confirmText?: string;
 }
 
-export interface LoginProps {
-  email: string;
-  password: string;
-}
-
 const LoginModal = ({ title, confirmText }: LoginModalProps) => {
   const { hideModal } = useModal();
   const windowsWIdth = useWidthResize();
-  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInState);
-  const navigate = useNavigate();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginProps>({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
 
   //useModal에서 빼온 hideModal을 사용해 모달백그라운드를 누르면 닫히게 설정함
   const onClose = () => {
     hideModal();
   };
 
-  const signInMutation = useMutation(onSubmitHandler, {
-    onSuccess: (res) => {
-      console.log('로그인 성공', res);
-      const userToken = {
-        authorization: res.headers.authorization,
-        'authorization-refresh': res.headers['authorization-refresh'],
-      };
-      if (res.status === 200) {
-        localStorage.setItem('userToken', JSON.stringify(userToken));
-        setIsLoggedIn(true);
-        hideModal();
-        navigate('/');
-      }
-    },
-    onError: (error) => {
-      console.log('error', error);
-    },
-  });
-
-  const submitHandler = (data: LoginProps) => {
-    signInMutation.mutate(data);
+  // form 태그의 특성에 따라 새로고침을 막기 위해 e.preventDefault 설정함
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
   };
 
   return (
@@ -86,20 +45,16 @@ const LoginModal = ({ title, confirmText }: LoginModalProps) => {
           />
         </div>
         <h1 className="pt-[1.25rem]">{title}</h1>
-        <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col">
+        <form onSubmit={onSubmitHandler} className="flex flex-col">
           <input
             type="email"
             className="placeholder:placeholder:text=[#909090] my-3 border-b-[.0625rem] border-[#909090] p-4 text-sm outline-none focus:border-[#9664FF]"
             placeholder="이메일을 입력해주세요"
-            id="email"
-            {...register('email')}
           />
           <input
             type="password"
             className="placeholder:placeholder:text=[#909090] my-3 border-b-[.0625rem] border-[#909090] p-4 text-sm outline-none focus:border-[#9664FF]"
             placeholder="비밀번호를 입력해주세요"
-            id="password"
-            {...register('password')}
           />
           <div className="flex flex-col items-center justify-center">
             <Button className={'btn-purple' + ' mb-2 bg-white text-[#838383]'}>
