926181102a7a543622de1d53f092462378c8e284
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-10-02 17:16:58 +0900

    Design: apply css
    - confirmModal의 기본 css 적용합니다
    
    by @Hah-nna

--- src/components/Modal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal.tsx
@@ -1,4 +1,5 @@
 import useModal from '../../hook/useModal';
+import Button from '../../UI/Button';
 import ModalBackground from '../../UI/ModalBackground';
 
 export interface ConfirmModalProps {
@@ -26,10 +27,16 @@ const ConfirmModal = ({
   return (
     <ModalBackground onClose={onClose}>
       <div className="modal-box">
-        <div>{title}</div>
-        <div>
-          <button onClick={onCancelClick}>{cancelText}</button>
-          <button onClick={onConfirmClick}>{confirmText}</button>
+        <div className="flex flex-col items-center justify-center">
+          <h2>{title}</h2>
+          <div className="mt-10 flex w-full justify-evenly">
+            <Button className="btn-purple px-3" onClick={onCancelClick}>
+              {cancelText}
+            </Button>
+            <Button className="btn-purple px-3" onClick={onConfirmClick}>
+              {confirmText}
+            </Button>
+          </div>
         </div>
       </div>
     </ModalBackground>
